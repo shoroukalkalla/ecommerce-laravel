874a4f45f0afd7265e028d3cc752cc0e6c42e2e4
unknown <shroukalkalla1997@gmail.com>
2022-08-21 22:12:21 +0200

    updated home views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+
+    public function index()
+    {
+        return view('home.userpage');
+    }
+
     public function redirect()
     {
         $userType = Auth::user()->user_type;
