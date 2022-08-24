3667fb291ca41973fb4d31a9c4f32c30d45ffcfd
unknown <shroukalkalla1997@gmail.com>
2022-08-24 14:20:17 +0200

    added category routes, view and controller

---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,52 @@
+{{-- <x-app-layout>
+</x-app-layout> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+
+    <style>
+        .div_center{
+            text-align: center;
+            padding top:40px ;
+        }
+
+        .h2_font{
+            font-size: 40px;
+            padding-bottom: 40px;
+        }
+
+        .input_color{
+            color: black;
+        }
+    </style>
+
+  </head>
+  <body>
+    <div class="container-scroller">
+      @include('admin.sidebar')
+
+      <div class="container-fluid page-body-wrapper">
+          @include('admin.navbar')
+
+          <div class="main-panel">
+            <div class="content-wrapper">
+                
+                <div class="div_center">
+                    <h2 class="h2_font">Add Category</h2>
+
+                    <form action="{{url('/add_category')}}" method="POST">
+                        @csrf
+                        <input type="text" placeholder="Enter category name" name="category_name" class="input_color">
+                        <input type="submit" value="Add Category" class="btn btn-primary">
+                    </form>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+
+    @include('admin.script')
+  </body>
+</html>
