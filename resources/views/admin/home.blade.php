cb61c16dd67362c4804112faa55a3e73836ca711
unknown <shroukalkalla1997@gmail.com>
2022-08-24 12:08:01 +0200

    added admin dashboard files

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,33 @@
-<x-app-layout>
+{{-- <x-app-layout>
+</x-app-layout> --}}
 
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_navbar.html -->
+        @include('admin.navbar')
+        <!-- partial -->
+        <div class="main-panel">
+         @include('admin.body')
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+          @include('admin.footer')
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script')
+  </body>
+</html>
